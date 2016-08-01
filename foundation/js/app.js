440e481995eb9ca26ffa28d9e6f295ef965b6e33
Hannah Roe <hannahdroe@gmail.com>
2016-08-01 13:42:54 -0600

    Some finishing touches. Addition of social footer on all pages.

--- foundation/js/app.js
+++ foundation/js/app.js
@@ -2,7 +2,3 @@ $(document).foundation();
 
 Foundation.Abide.defaults.patterns['alpha_numeric'] = /^[a-zA-Z0-9 \s.!?-]*$/;
 Foundation.Abide.defaults.patterns['alpha'] =/^[a-zA-Z \s]+$/;
-$("#contact-form").on("formvalid.zf.abide", function(ev,frm) {
-    ev.preventDefault();
-  alert("Form is valid, finally!");
-});
